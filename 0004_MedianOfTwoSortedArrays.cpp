ab0c00e705f8a440a6299291861a8b42350c315f
joy.yang <yangjiali94@126.com>
2021-04-15 14:45:28 +0800

    change 0004

--- 0004_MedianOfTwoSortedArrays.cpp
+++ 0004_MedianOfTwoSortedArrays.cpp
@@ -1,3 +1,5 @@
+/*
+二分法，但是分多不是数组，而是k，每次删除k/2个小数
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -31,3 +33,35 @@ public:
     }
     
 };
+*/
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+        int k1=(m+n+1)/2;
+        int k2=(m+n+2)/2;
+        return (findK(nums1,0,nums2,0,k1)+findK(nums1,0,nums2,0,k2)) / 2.0;
+        
+    }
+    int findK(vector<int>& nums1,int start1,vector<int>& nums2,int start2,int k)
+    {
+        if(start1>=nums1.size()) return nums2[start2+k-1];
+        if(start2>=nums2.size()) return nums1[start1+k-1];
+        if(k==1) return nums1[start1]>nums2[start2]?nums2[start2]:nums1[start1];
+
+        if((start1+k/2)>nums1.size()) 
+            return findK(nums1,start1,nums2,start2+k/2,k-k/2);
+        if((start2+k/2)>nums2.size()) 
+            return findK(nums1,start1+k/2,nums2,start2,k-k/2);
+
+        if (nums1[start1+k/2-1]>nums2[start2+k/2-1]) 
+            return findK(nums1,start1,nums2,start2+k/2,k-k/2);
+        else 
+            return findK(nums1,start1+k/2,nums2,start2,k-k/2);
+
+
+    }
+
+};
+
