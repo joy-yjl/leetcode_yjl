308c8bb94acf782bf2285b89bc6bb375ec0f7027
joy.yang <yangjiali94@126.com>
2021-04-19 17:55:19 +0800

    change 0006

--- 0006_ZigZagConversion.cpp
+++ 0006_ZigZagConversion.cpp
@@ -1,22 +1,29 @@
+/*
+单纯的找规律
+*/
+
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows<=1) return s;
-        int n=s.size();
-        int size=numRows*2-2;
-        string res;
-        for(int i=0;i<numRows;i++)
-        {
-            for(int j=i;j<n;j+=size)
-            {
-                res+=s[j];
-                int pose=j+size-i*2;
-                if(i!=0&&i!=numRows-1&&pose<n)
-                    res+=s[pose];
-            }
-        }
+       int n=s.size();
+       if (numRows<=1) return s;
+       int group=(numRows-1)*2;
+       string res="";
+       for(int i=0;i<numRows;i++)
+       {
+           for(int j=i;j<n;j=j+group)
+           {
+               res+=s[j];
+               int pos=j+group-i*2;
+               if(i!=0 && i!=numRows-1 && pos<n )
+               {
+                   res+=s[pos];
+               }
+
+           }
+
+       }
         return res;
-        
-        
+
     }
 };
