1f3787e238d3ebdfa8f7fce7a670c6ce2c190688
joy.yang <yangjiali94@126.com>
2021-12-07 10:51:39 +0800

    update cpp 0003

--- cpp/0003_LongestSubstringWithoutRepeatingCharacters.cpp
+++ cpp/0003_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -11,16 +11,16 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char,int> hash;
-        int start=0;
+        int start=-1;
         int res=0;
         for(int i=0;i<s.size();i++)
         {
             if(hash.count(s[i])!=0)
             {
-                start=start>hash[s[i]]?start:hash[s[i]]+1;
+                start=start>hash[s[i]]?start:hash[s[i]];
             }
             hash[s[i]]=i;
-            res=res>(i-start+1)?res:(i-start+1);
+            res=res>(i-start)?res:(i-start);
         }
         return res;
         
