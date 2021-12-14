c15f03faf8d57a57f89a6ba8c2fb23be7b73b32a
joy.yang <yangjiali94@126.com>
2021-12-14 15:04:13 +0800

    add python  0013

--- cpp/0013_RomantoInteger.cpp
+++ cpp/0013_RomantoInteger.cpp
@@ -8,7 +8,7 @@ public:
     	unordered_map<char,int> dict{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
     	int res=0;
     	int n=s.size();
-    	int max=dict[s[n-1]];
+    	int max=0;
     	for(int i=n-1;i>=0;i--)
     	{
     		int tmp=dict[s[i]];
