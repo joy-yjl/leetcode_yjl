fcd26240f45d8d28feed31fdaa8fb4c6e8862c44
joy.yang <yangjiali94@126.com>
2022-01-27 04:44:24 +0800

    add 0045 cpp

---
+++ cpp/0045_JumpGameII.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size()<2)
+            return 0;
+        int res=0,index=0;
+        while(index<nums.size())
+        {
+            int best_index=-1,best_step=-1;
+            if(nums[index]>=(nums.size()-1-index))
+            {
+                return res+1;
+            }
+            for(int i=1;i<=nums[index];i++)
+            {
+                int step=nums[index+i]-(nums[index]-i);
+                if(step>best_step)
+                {
+                    best_step=step;
+                    best_index=index+i;
+                }
+            }
+            index=best_index;
+            res++;
+        }
+        return res;    
+    }
+};
