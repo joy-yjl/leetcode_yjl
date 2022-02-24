a454c6d562b99aca3ffeda1ff94bec5f35f3935f
joy.yang <yangjiali94@126.com>
2022-02-24 14:50:21 +0800

    change 0538 cpp

--- cpp/0538_ConvertBSTtoGreaterTree.cpp
+++ cpp/0538_ConvertBSTtoGreaterTree.cpp
@@ -38,3 +38,30 @@ public:
         return root;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    
+    TreeNode* convertBST(TreeNode* root) {
+        if(root==nullptr)
+            return nullptr;
+        convertBST(root->right);
+        sum=sum+root->val;
+        root->val=sum;
+        convertBST(root->left);
+        return root;
+    }
+};
