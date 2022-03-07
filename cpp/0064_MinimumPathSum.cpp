2195516cab5949901527cd31263632310e2be8bb
joy.yang <yangjiali94@126.com>
2022-03-07 16:18:15 +0800

    add 64 cpp

---
+++ cpp/0064_MinimumPathSum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int rows=grid.size();
+        if(rows==0)
+            return 0;
+        int cols=grid[0].size();
+        for(int i=1;i<cols;i++)
+            grid[0][i]=grid[0][i-1]+grid[0][i];
+        for(int i=1;i<rows;i++)
+            grid[i][0]=grid[i-1][0]+grid[i][0];
+        for(int i=1;i<rows;i++)
+        {
+            for(int j=1;j<cols;j++)
+            {
+                    grid[i][j]=min(grid[i-1][j],grid[i][j-1])+grid[i][j];
+            }
+        }
+        return grid[rows-1][cols-1];
+    }
+};
