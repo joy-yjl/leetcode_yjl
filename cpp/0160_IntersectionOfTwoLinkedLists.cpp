7812e6d128176bc2f4d8a33ebb07e714167d713a
joy.yang <yangjiali94@126.com>
2022-03-25 18:09:38 +0800

    add 160

---
+++ cpp/0160_IntersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode * curA=headA;
+        ListNode * curB=headB;
+        while(curA!=curB)
+        {
+            if(curA!=nullptr)
+                curA=curA->next;
+            else
+                curA=headB;
+            if(curB!=nullptr)
+                curB=curB->next;
+            else
+                curB=headA;
+        }
+        return curA;
+        
+    }
+};
