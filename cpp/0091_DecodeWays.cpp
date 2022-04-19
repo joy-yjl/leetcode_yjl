ce065af89250452b0e69d037e7621652c5cc9f5e
joy.yang <yangjiali94@126.com>
2022-04-19 16:38:03 +0800

    add 91

---
+++ cpp/0091_DecodeWays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.size();
+        if(n==0)
+            return 0;
+        if(s[0]<='0' || s[0]>'9')
+        {
+            return 0;
+        }
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=1;i<n;i++)
+        {
+            if(s[i]<'0' || s[i]>'9')
+                return 0;
+            if((s[i-1]=='0' || s[i-1]>'2')&& s[i]=='0' )
+                return 0;
+            if(s[i-1]=='0' ||s[i-1] >'2' || (s[i-1]=='2'&&s[i]>'6'))
+                dp[i+1]=dp[i];
+            else if(s[i]=='0')
+                dp[i+1]=dp[i-1];
+            else 
+                dp[i+1]=dp[i]+dp[i-1];
+
+        }
+        return dp[n];
+
+        
+    }
+};
