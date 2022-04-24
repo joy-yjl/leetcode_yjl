f48f957aa7fdf346403ec836093cd5a30cdd52ad
joy.yang <yangjiali94@126.com>
2022-04-24 16:22:43 +0800

    add 109

---
+++ cpp/0109_ConvertSortedListToBinarySearchTree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode * helper(ListNode * head,ListNode * tail)
+    {
+        if(head==tail)
+            return nullptr;
+        ListNode * fast=head, *slow=head;
+        while(fast!=tail && fast->next!=tail)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        TreeNode * root=new TreeNode(slow->val);
+        root->left=helper(head,slow);
+        root->right=helper(slow->next,tail);
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        TreeNode * root=helper(head,nullptr);
+        return root;
+        
+    }
+};
